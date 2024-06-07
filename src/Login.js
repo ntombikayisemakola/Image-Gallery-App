ac95f0af1e555efc12762687cd030237c5dd1b48
Ntombikayise Kidibone Makola <ntombikayise.makola@gmail.com>
2024-06-07 12:48:39 +0200

    Registration and Login are JS are done, only left with styling

--- src/Login.js
+++ src/Login.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
+import axios from "axios";
 
 function Login(){
+    const [emailAddress, setEmailAddress] = useState('');
+    const [password, setPassword] = useState('');
 
+    const handleEmailAddressChange = (value) => {
+        setEmailAddress(value);
+    }
+    const handlePasswordChange = (value) => {
+        setPassword(value);
+    }
+    
+    const handleLogin = () => {
+        const data = {
+        EmailAddress : emailAddress,
+        Password : password,
+     };
+
+     const url = 'https://localhost:44308/api/Test/Login';
+     axios.post(url, data).then((result) =>{
+        alert(result.data);
+     }).catch((error)=>{
+        alert(error);
+     })
+    }
 
     return(
-        <div>Login</div>
+        <Fragment>
+            <label>Email Address</label>
+        <input type="text" id="txtEmailAddress" placeholder='Enter Email Address' onChange={(e) => handleEmailAddressChange(e.target.value)} /><br></br>
+        <label>Password</label>
+        <input type="text" id="txtPassword" placeholder='Enter Password' onChange={(e) => handlePasswordChange(e.target.value)} /><br></br>
+        <br></br>
+        <button onClick={() => handleLogin()}>Login</button>
+        </Fragment>
     )
 }
 
